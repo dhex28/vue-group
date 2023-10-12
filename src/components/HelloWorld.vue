bccdcd513f9ea42e4a62f0b02f09e0292eb49dec
dhex28 <dextherbdelmonte@gmail.com>
2023-10-12 18:39:47 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand text-white" href="#">Lowkey Dev</a>
+        <a class="navbar-brand text-white" href="#">Dev</a>
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
